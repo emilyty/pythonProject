6316095a0c6fe8d6c1df34e4412645bdd478e40d
emily <emilyty@163.com>
2018-06-20 16:50:00 +0800

    删除没用的测试文件

--- analysis/analysisLog/loyaltyLP3/analysisLogLoyalty.py
+++ analysis/analysisLog/loyaltyLP3/analysisLogLoyalty.py
@@ -1,5 +1,4 @@
 # _*_ coding:utf-8 _*_
-# -*- coding: utf-8 -*-
 # @Author: emily
 # @Date  : 2018/3/20 13:52
 # @Contact : emilyty@163.com
--- testingJob/rrr.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding:utf-8 _*_
-# -*- coding: utf-8 -*-
-# @Author: emily
-# @Date  : 2018/3/20 13:52
-# @Contact : emilyty@163.com
-# @Desc:
-if __name__ == '__main__':
-    pass
